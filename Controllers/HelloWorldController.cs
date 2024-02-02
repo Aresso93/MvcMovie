9de5d61a4da5dbdbe409927e4555f6721c04b4f0
Aresso93 <135212764+Aresso93@users.noreply.github.com>
2024-02-02 12:59:30 +0100

    Initial commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System.Xml.Linq;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        //public string Welcome()
+        //{
+        //    return "This is the Welcome action method...";
+        //}
+
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        //}
+    }
+}
