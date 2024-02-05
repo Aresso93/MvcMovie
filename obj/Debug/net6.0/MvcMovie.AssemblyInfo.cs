c72675138d8b518001d0ee68e0a9b0d1670b2d7d
Aresso93 <135212764+Aresso93@users.noreply.github.com>
2024-02-05 18:08:07 +0100

    modifiche, tentativo di validazione custom

--- obj/Debug/net6.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net6.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9de5d61a4da5dbdbe409927e4555f6721c04b4f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
