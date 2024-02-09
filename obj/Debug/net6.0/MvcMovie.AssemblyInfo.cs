4f2b69b894d7e02c475361546c407e978b564d8a
Aresso93 <135212764+Aresso93@users.noreply.github.com>
2024-02-09 12:29:39 +0100

    /

--- obj/Debug/net6.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net6.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c249918e1414636a7287e2c83e3856948e6d767")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b8f0823b1c35d4895e4b317bf5e8b7af3b3c65d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
